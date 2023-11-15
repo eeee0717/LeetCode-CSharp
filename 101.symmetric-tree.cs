0f4c2bdd6c690566d1c7d449f9b13feafa1dba05
eeee0717 <70054568+eeee0717@users.noreply.github.com>
2023-11-15 09:16:14 +0800

    2023-11-15

--- 101.symmetric-tree.cs
+++ 101.symmetric-tree.cs
@@ -25,22 +25,46 @@
  */
 public class Solution
 {
+    // public bool IsSymmetric(TreeNode root)
+    // {
+    //     if (root == null) return true;
+    //     return Compare(root.left, root.right);
+    // }
+    // public bool Compare(TreeNode left, TreeNode right)
+    // {
+    //     if(left == null && right != null) return false;
+    //     else if(left != null && right == null ) return false;
+    //     else if(left == null && right == null) return true;
+    //     else if(left.val != right.val) return false;
+
+    //     var outside = Compare(left.left, right.right);
+    //     var inside = Compare(left.right, right.left);
+
+    //     return outside&&inside;
+    // }
+
     public bool IsSymmetric(TreeNode root)
     {
         if (root == null) return true;
-        return Compare(root.left, root.right);
-    }
-    public bool Compare(TreeNode left, TreeNode right)
-    {
-        if(left == null && right != null) return false;
-        else if(left != null && right == null ) return false;
-        else if(left == null && right == null) return true;
-        else if(left.val != right.val) return false;
+        var st = new Stack<TreeNode>();
+        st.Push(root.left);
+        st.Push(root.right);
+        while (st.Count != 0)
+        {
+            var left = st.Pop();
+            var right = st.Pop();
+            if (left == null && right == null)
+                continue;
 
-        var outside = Compare(left.left, right.right);
-        var inside = Compare(left.right, right.left);
+            if ((left == null || right == null || (left.val != right.val)))
+                return false;
 
-        return outside&&inside;
+            st.Push(left.left);
+            st.Push(right.right);
+            st.Push(left.right);
+            st.Push(right.left);
+        }
+        return true;
     }
 }
 // @lc code=end
