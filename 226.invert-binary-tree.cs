f8c0b0f2bf4491296633e6f4aa86e0e2a8ac0698
eeee0717 <70054568+eeee0717@users.noreply.github.com>
2023-11-15 08:56:19 +0800

    2023-11-15

---
+++ 226.invert-binary-tree.cs
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=csharp
+ * @lcpr version=30109
+ *
+ * [226] 翻转二叉树
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode InvertTree(TreeNode root) {
+        if(root == null) return root;
+        (root.left,root.right) = (root.right, root.left);
+        InvertTree(root.left);
+        InvertTree(root.right);
+        return root;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [4,2,7,1,3,6,9]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [2,1,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
+
