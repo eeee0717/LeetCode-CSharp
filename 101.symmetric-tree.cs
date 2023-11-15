94276cf12ee2b97e66e94eb726b0b44ee5708211
eeee0717 <70054568+eeee0717@users.noreply.github.com>
2023-11-15 09:07:07 +0800

    2023-11-15

---
+++ 101.symmetric-tree.cs
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=csharp
+ * @lcpr version=30109
+ *
+ * [101] 对称二叉树
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public bool IsSymmetric(TreeNode root)
+    {
+        if (root == null) return true;
+        return Compare(root.left, root.right);
+    }
+    public bool Compare(TreeNode left, TreeNode right)
+    {
+        if(left == null && right != null) return false;
+        else if(left != null && right == null ) return false;
+        else if(left == null && right == null) return true;
+        else if(left.val != right.val) return false;
+
+        var outside = Compare(left.left, right.right);
+        var inside = Compare(left.right, right.left);
+
+        return outside&&inside;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,2,3,4,4,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,2,null,3,null,3]\n
+// @lcpr case=end
+
+ */
+
