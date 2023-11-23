be1c25fb3bac9b6723b203d3d03386a682744beb
eeee0717 <70054568+eeee0717@users.noreply.github.com>
2023-11-23 09:38:38 +0800

    2023-11-23

---
+++ 112.path-sum.cs
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=csharp
+ * @lcpr version=30110
+ *
+ * [112] 路径总和
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public bool HasPathSum(TreeNode root, int targetSum)
+    {
+        if (root == null) return false;
+        if (root.left == null && root.right == null && targetSum == root.val) return true;
+        return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [5,4,8,11,null,13,4,7,2,null,null,null,1]\n22\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3]\n5\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n0\n
+// @lcpr case=end
+
+ */
+
