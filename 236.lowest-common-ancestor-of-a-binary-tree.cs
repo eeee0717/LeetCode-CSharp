2e904adfcb34f50c0996689cafd724fde79ba2bc
eeee0717 <chentao020717Work@outlook.com>
2023-12-04 09:43:16 +0800

    2023-12-04

---
+++ 236.lowest-common-ancestor-of-a-binary-tree.cs
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=csharp
+ * @lcpr version=30111
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = LowestCommonAncestor(root.left, p, q);
+        TreeNode right = LowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root;
+
+        if (left == null && right != null) return right;
+        if (left != null && right == null) return left;
+        return null;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,5,1,6,2,0,8,null,null,7,4]\n5\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,5,1,6,2,0,8,null,null,7,4]\n5\n4\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n1\n2\n
+// @lcpr case=end
+
+ */
+
