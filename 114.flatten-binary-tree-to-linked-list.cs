097574a4b49d8759f6ed0d539eba062db98bfba2
eeee0717 <chentao020717Work@outlook.com>
2024-05-11 09:31:18 +0800

    2024-05-11

---
+++ 114.flatten-binary-tree-to-linked-list.cs
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=114 lang=csharp
+ * @lcpr version=30201
+ *
+ * [114] 二叉树展开为链表
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public void Flatten(TreeNode root)
+    {
+        while (root != null)
+        {
+            if (root.left == null)
+            {
+                root = root.right;
+            }
+            else
+            {
+                var pre = root.left;
+                while (pre.right != null)
+                {
+                    pre = pre.right;
+                }
+                pre.right = root.right;
+                root.right = root.left;
+                root.left = null;
+                root = root.right;
+            }
+        }
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,5,3,4,null,6]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0]\n
+// @lcpr case=end
+
+ */
+
