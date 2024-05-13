bd063483b857ea8512fb9a2881077768e4fca071
eeee0717 <chentao020717Work@outlook.com>
2024-05-13 09:24:01 +0800

    2024-05-13

---
+++ 1143.longest-common-subsequence.cs
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=1143 lang=csharp
+ * @lcpr version=30201
+ *
+ * [1143] 最长公共子序列
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+public class Solution
+{
+    public int LongestCommonSubsequence(string text1, string text2)
+    {
+        int m = text1.Length;
+        int n = text2.Length;
+        int[,] dp = new int[m + 1, n + 1];
+        dp[0,0] = 0;
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (text1[i - 1] == text2[j - 1])
+                {
+                    dp[i, j] = dp[i - 1, j - 1] + 1;
+                }
+                else
+                {
+                    dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+                }
+            }
+        }
+        return dp[m,n];
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "abcde"\n"ace"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "abc"\n"abc"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "abc"\n"def"\n
+// @lcpr case=end
+
+ */
+
